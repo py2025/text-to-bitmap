7defbdca8ab4f4c5b5b21bb3b59cd80b325eb57f
Paul Yang <py2025@gmail.com>
2021-02-16 20:05:53 -0500

    add printImg

--- Image.cpp
+++ Image.cpp
@@ -12,3 +12,23 @@ Pixel pullPixel(Image *img, int row, int column) {
             *Matrix_at(&(img->pixelB), row, column)};
     return p;
 }
+
+void image_init(Image *img, int width, int height) {
+    img->width = width;
+    img->height = height;
+    Matrix_init(&(img->pixelR), width, height);
+    Matrix_init(&(img->pixelG), width, height);
+    Matrix_init(&(img->pixelB), width, height);
+}
+
+int getImgHeight(Image *img){
+    return img->height;
+}
+
+int getImgWidth(Image *img){
+    return img->width;
+}
+
+int printImg(Image *img, ostream& os){
+    
+}
