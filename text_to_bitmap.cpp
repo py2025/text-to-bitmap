e4d2d0be07202966cd5180edba3cfd9803152ebb
Playingza1285 <williamchen123321@gmail.com>
2021-02-16 21:16:08 -0500

    bitmap

---
+++ text_to_bitmap.cpp
@@ -0,0 +1,7 @@
+#include "text_to_bitmap.h";
+
+namespace fs = std::filesystem;
+
+void create_image(Image *img, ifstream& is, string file_name) {
+    fs::path p {file_name};
+}
--- text_to_bitmap.h
+++ text_to_bitmap.h
@@ -4,9 +4,15 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
+
+#include "Matrix.h"
+#include "Image.h"
 
 using namespace std;
 
-
+void text_conversion(string file_name);
+void create_image(Image *img, ifstream& is, string file_name);
+void set_pixel(Image *img, int r, int g, int b);
 
 #endif
